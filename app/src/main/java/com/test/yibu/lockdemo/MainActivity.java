1eb37625e4c7eb62b481123b95045ffe01036ba5
zhengcf <zhengcaifu@1bgx.net>
2017-11-22 14:55:17 +0800

    lib

--- app/src/main/java/com/test/yibu/lockdemo/MainActivity.java
+++ app/src/main/java/com/test/yibu/lockdemo/MainActivity.java
@@ -1,44 +1,75 @@
 package com.test.yibu.lockdemo;
 
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.text.format.DateUtils;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.permissionlib.util.AndroidMPermissionActivity;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.MapView;
 import com.permissionlib.util.AndroidMPermissionHelper;
 import com.test.yibu.lockdemo.bean.LockMessage;
 import com.test.yibu.lockdemo.lock.lock.LockConstant;
 import com.test.yibu.lockdemo.lock.lock.LockManager;
 import com.test.yibu.lockdemo.lock.lock.linister.LockCloseListener;
 import com.test.yibu.lockdemo.lock.lock.linister.OpenLockListener;
+import com.test.yibu.lockdemo.util.DateTimeUtil;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import butterknife.BindView;
 import butterknife.OnClick;
 
 public class MainActivity extends BaseActivity {
 
-    @BindView(R.id.hint_tv)
-    TextView hintTv;
+    @BindView(R.id.map_view)
+    MapView mapView;
+    @BindView(R.id.tv_cycling_time)
+    TextView tvCyclingTime;
+    @BindView(R.id.scan_btn)
+    View vScanBtn;
+    @BindView(R.id.cycling_layout)
+    View vCycling;
 
     LockManager lockManager;
+
+    Dialog loadingDialog;
+
+    Timer timer;
+    Long startTime;
+    Animation inAnim, outAnim;
 
     @Override
     public int getLayoutRes() {
         return R.layout.activity_main;
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initMap(savedInstanceState);
+        inAnim = AnimationUtils.loadAnimation(this, R.anim.slide_bottom_in);
+        outAnim = AnimationUtils.loadAnimation(this, R.anim.slide_bottom_out);
+    }
 
-    @OnClick({R.id.scan_btn, R.id.cancel_btn})
+    @OnClick({R.id.scan_btn, R.id.stop_tv})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.scan_btn:
                 checkScanPermission();
                 break;
-            case R.id.cancel_btn:
-                LockManager.getInstance(this).cancelOption();
-                hintTv.setText("");
+            case R.id.stop_tv:
+                endCycling();
                 break;
+
         }
     }
 
@@ -67,17 +98,19 @@ public class MainActivity extends BaseActivity {
     }
 
     private void openLock() {
+        loadingDialog = ProgressDialog.show(this, "", getString(R.string.str_opening), true, false);
         final LockMessage lockMessage = new LockMessage("3C:A3:08:08:C5:E7", LockConstant.INIT_PWD, LockConstant.INIT_KEY);
-        hintTv.setText(getString(R.string.str_opening));
+        loadingDialog.show();
         lockManager = LockManager.getInstance(this);
         lockManager.openLock(lockMessage, new OpenLockListener() {
             @Override
             public void onOpenLock(boolean success, String message) {
+                loadingDialog.dismiss();
                 if (success) {
-                    hintTv.setText(getString(R.string.str_open_lock_success));
+                    startCycling();
                     monitorClose(lockMessage);
                 } else {
-                    hintTv.setText(getString(R.string.str_open_lock_fail, message));
+                    Toast.makeText(MainActivity.this, getString(R.string.str_open_lock_fail, message), Toast.LENGTH_LONG).show();
                     if (LockConstant.LOCK_TYPE_ALREADY_OPEN.equals(message)) {
                         monitorClose(lockMessage);
                     }
@@ -90,8 +123,53 @@ public class MainActivity extends BaseActivity {
         lockManager.monitoringLockClose(lockMessage, new LockCloseListener() {
             @Override
             public void onLockClose(boolean state) {
-                hintTv.setText(getString(R.string.str_close_lock_success));
+                Toast.makeText(MainActivity.this, getString(R.string.str_close_lock_success), Toast.LENGTH_LONG).show();
+                endCycling();
             }
         });
+    }
+
+    private void initMap(@Nullable Bundle savedInstanceState) {
+        mapView.onCreate(savedInstanceState);// 此方法必须重写
+        AMap aMap = mapView.getMap();
+        aMap.setTrafficEnabled(true);// 显示实时交通状况
+        //地图模式可选类型：MAP_TYPE_NORMAL,MAP_TYPE_SATELLITE,MAP_TYPE_NIGHT
+        aMap.setMapType(AMap.MAP_TYPE_NORMAL);
+    }
+
+    private void startCycling() {
+        vScanBtn.setVisibility(View.GONE);
+        vCycling.setVisibility(View.VISIBLE);
+        vCycling.setAnimation(inAnim);
+        inAnim.start();
+        startTime = System.currentTimeMillis();
+        timer = new Timer();
+        startTime = 0l;
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        tvCyclingTime.setText(DateTimeUtil.formatLong2TimeStr(startTime));
+                        startTime += 1000;
+                    }
+                });
+            }
+        }, 0, 1000);
+    }
+
+    /**
+     * 结束计费
+     */
+    private void endCycling() {
+        vScanBtn.setVisibility(View.VISIBLE);
+        vCycling.setVisibility(View.GONE);
+        vCycling.setAnimation(outAnim);
+        outAnim.start();
+        if (timer != null) {
+            timer.cancel();
+            timer = null;
+        }
     }
 }
